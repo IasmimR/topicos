286dcc9afde28552cfe836c657fe369c9d127d01
brunocesarsilva <bruno@brunocesar.com>
2013-11-20 10:39:24 -0200

    Correção em documentação de pacotes

--- src/main/java/br/ufg/inf/rdf/model/package-info.java
+++ src/main/java/br/ufg/inf/rdf/model/package-info.java
@@ -1,4 +1,7 @@
 /**
- * Entidades do modelo do RDF, utilizando FOAF e RELATIONSHIP
+ * Entidades do modelo do RDF, utilizando FOAF e ACM CS
+ *
+ * @created 19/10/2013
+ * @author Bruno César Ribeiro e Silva - <a href="mailto:bruno@brunocesar.com">bruno@brunocesar.com</a>
  */
 package br.ufg.inf.rdf.model;
